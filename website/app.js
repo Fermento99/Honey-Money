09e2d09beb39b059e0a8aadc9e775de31d7c5577
Fermento99 <pk.pawlak99@gmail.com>
2021-01-04 21:46:32 +0100

    logout

--- website/app.js
+++ website/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const dbmanager = require('./dbmanager');
+const { nextTick } = require('process');
 
 app = express();
 const port = 3000;
@@ -15,67 +16,89 @@ app.use(express.json())
 let refreshTokens = [];
 
 let generateAccessToken = user => {
-    return jwt.sign({user: user, isDate: Date.now()}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '15m'});
+  return jwt.sign({ user: user, isDate: Date.now() }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+}
+
+let generateRefreshToken = user => {
+  return jwt.sign({ user: user, isDate: Date.now() }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '15m' });
+}
+
+let checkToken = token => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+    if(err) {
+      coneole.log(err);
+      next(false);
+    } else next(token.user)
+  });
 }
 
 
 app.get('/', (req, res) => {
-    res.redirect('/welcome');
+  res.redirect('/welcome');
 })
 
 app.get('/welcome', (req, res) => {
-    res.sendFile(__dirname + "/static/index.html");
+  res.sendFile(__dirname + "/static/index.html");
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + "/static/login_page.html");
+  res.sendFile(__dirname + "/static/login_page.html");
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(__dirname + "/static/register_page.html");
+  res.sendFile(__dirname + "/static/register_page.html");
 })
 
 app.get('/main', (req, res) => {
-    res.sendFile(__dirname + "/static/main_page.html");
+  res.sendFile(__dirname + "/static/main_page.html");
 })
 
 app.post('/register', (req, res) => {
-    dbmanager.register(req.body, res);
+  dbmanager.register(req.body, res);
 })
 
-app.get('/register_successful', (req,res) => {
-    res.sendFile(__dirname + "/static/register_successful.html")
+app.get('/register_successful', (req, res) => {
+  res.sendFile(__dirname + "/static/register_successful.html")
 })
 
 
 app.post('/auth/login', (req, res) => {
-    let user = req.body.login;
-    let refreshToken = generateAccessToken(user);
-    res.cookie("refresh_token", refreshToken, {httpOnly: true, maxAge: 900000, sameSite: true});
-    refreshTokens.push(refreshToken);
-    console.log(refreshTokens);
-    res.sendStatus(200);
-    // res.sendStatus('401')
+  dbmanager.login(req.body, success => {
+    if (success) {
+      let user = req.body.login;
+      let refreshToken = generateRefreshToken(user);
+      res.cookie("refresh_token", refreshToken, { httpOnly: true, maxAge: 900000, sameSite: true });
+      refreshTokens.push(refreshToken);
+      console.log('new login');
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(401);
+    }
+  });
 });
 
 app.delete('/auth/logout', (req, res) => {
-    let t = req.cookies.token;
-    if(t && refreshTokens.includes(t)) ;
+  let t = req.cookies.token;
+  if(t && refreshTokens.includes(t)) {
+    refreshTokens.splice(refreshTokens.indexOf(t));
+    res.sendStatus(200);
+  } else res.sendStatus(400);
 });
 
 app.post('/auth/refresh', (req, res) => {
-    let t = req.cookies.refresh_token;
-    if(!t) res.sendStatus(401);
-    else if(!refreshTokens.includes(t)) res.sendStatus(403);
-    else {
-        jwt.verify(t, process.env.REFRESH_TOKEN_SECRET, (err, token) => {
-            if(err) res.sendStatus(403);
-            else {
-                accessToken = generateAccessToken(token.user);
-                res.json({username: token.user, token: accessToken});
-            }
-        })
-    }
+  let t = req.cookies.refresh_token;
+  if (!t) res.sendStatus(401);
+  else if (!refreshTokens.includes(t)) res.sendStatus(403);
+  else {
+    jwt.verify(t, process.env.REFRESH_TOKEN_SECRET, (err, token) => {
+      if (err) res.sendStatus(403);
+      else {
+        accessToken = generateAccessToken(token.user);
+        res.cookie("refresh_token", t, { httpOnly: true, maxAge: 900000, sameSite: true });
+        res.json({ username: token.user, token: accessToken });
+      }
+    })
+  }
 });
 
 
--- website/dbmanager.js
+++ website/dbmanager.js
@@ -5,36 +5,61 @@ const bcrypt = require("bcrypt");
 
 
 const connect = () => {
-    return mysql.createConnection({
-      host: 'localhost',
-      user: 'gucio',
-      password: process.env.DATABASE_PASSWORD,
-      database: 'HoneyMoney'
-    })
-  }
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'gucio',
+    password: process.env.DATABASE_PASSWORD,
+    database: 'HoneyMoney'
+  })
+}
+
+function validateUser(login) {
+  if(login.length < 3 || login.length > 25 || /\s/.test(login)) return true;
+  return true;
+}
 
 
 function register(user, response) {
-    const {login, password} = user;
-    // validate
+  const { login, password } = user;
+  // validate
 
-    const salt = bcrypt.genSaltSync();
-    const hash = bcrypt.hashSync(password, salt);
-    let connection = connect();
-    connection.query('INSERT INTO users (username, pass) values (?, ?);', [login, hash], (err, res) => {
-        if(err){
-            console.log(err);
-            response.sendStatus(400);
-        } else {
-            console.log('ok');
-            response.sendStatus(200);
-        }
-    })
-    connection.end();
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  let connection = connect();
+  connection.query('INSERT INTO users (username, pass) values (?, ?);', [login, hash], (err, res) => {
+    if (err) {
+      console.log(err);
+      response.sendStatus(400);
+    } else {
+      console.log('ok');
+      response.sendStatus(200);
+    }
+  })
+  connection.end();
 };
 
-function login(user) {
-    const {username, pass} = user;
+function login(user, next) {
+  const { login, password } = user;
+  if(!validateUser(login)) return next(false);
+
+  let connection = connect();
+  connection.query('SELECT pass FROM users WHERE username = ?', login, (err, res) => {
+    if(err) {
+      console.log(err);
+      next(false);
+    }
+    else {
+      if(res[0])
+        if(bcrypt.compareSync(password, res[0].pass))
+          next(true);
+        else
+          next(false);
+      else
+        next(false);
+    }
+    connection.end();
+  })
+  
 }
 
 module.exports.register = register;
